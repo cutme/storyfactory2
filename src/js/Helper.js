bb44c3c2cf31a347b34fa96b7865895ff9cfd0a9
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2017-06-21 12:26:01 +0200

    Navigation & anims

--- src/js/Helper.js
+++ src/js/Helper.js
@@ -6,7 +6,9 @@
         	exist: exist,
         	blazy: blazy,
         	goToTarget: goToTarget,
-        	isWindowSmallerThan: isWindowSmallerThan
+        	isInView: isInView,
+        	isWindowSmallerThan: isWindowSmallerThan,
+        	showOnScroll: showOnScroll
         };
     };
     
@@ -34,20 +36,49 @@
 	
 	function goToTarget(target, offset) {
 		var $viewport = $('body, html'), 
-			o = $(target).offset().top;
+			o = $(target).offset().top,
+			page_offset = $(document).scrollTop(),
+			offset_diff = Math.abs(o - page_offset),
+			base_speed  = 500,
+			speed       = (offset_diff * base_speed) / 1000;
 			
-		//offset = offset || 0;		
+		offset = offset || 0;		
 
 		$viewport.animate({
-			scrollTop: o + offset
+			scrollTop: o + offset + 2
 		}, {
 			easing: 'easeOutCubic',
-			duration: 500
+			duration: speed
+		});
+	}
+	
+	function isInView() {
+		var bottomOfWindow = $(window).scrollTop() + $(window).height();
+
+		$('.anim').each(function() {
+			if ( $(this).offset().top < bottomOfWindow ) {
+				$(this).addClass('anim--done');
+			}
 		});
 	}
 	
 	function isWindowSmallerThan(resBorder) {
         return window.innerWidth < parseInt(resBorder, 10);
+    }
+    
+    function showOnScroll() {
+    	var body = $(window);
+		
+		body.on('scroll', function() {		
+	        $('.anim').each( function(){
+				var bottomOfObject = $(this).offset().top,
+		          	bottomOfWindow = $(window).scrollTop() + $(window).height();
+	
+				if( bottomOfWindow > bottomOfObject ) {
+					$(this).addClass('anim--done');
+				}
+			});
+		});
     }
 	
 	story.Helper = new Helper();
--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,22 @@
 	$(document).ready(function() {
 		
 		if (story.Helper.exist('.b-lazy')) story.Helper.blazy();
-
+		
+		
+		
+		$('.js-goto').on('click', function(e) {
+			e.preventDefault();
+			story.Helper.goToTarget($(this).attr('href'));
+			
+		});
 	});
 
+
+	$(window).on('load', function() {
+		story.Helper.isInView();
+		
+		story.Helper.showOnScroll();
+	});
 
 		
 }(window, document, jQuery, window.story = window.story || {}));
